cb8eed3c554773ecbddbec472122ba9fffd63447
David Melo <David.13rodrigues@hotmail.com>
2020-06-07 21:44:52 -0300

    Update 2 menu

--- app/src/main/java/com/example/mylistfavorite/LoginActivity.kt
+++ app/src/main/java/com/example/mylistfavorite/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mylistfavorite
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -9,6 +11,18 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        button.setOnClickListener(){
+
+            goToForgotPass()
+        }
+
+
+    }
+
+    private fun goToForgotPass(){
+
+        val intent= Intent(this@LoginActivity,HomeActivity::class.java)
+        startActivity(intent)
 
     }
 }
