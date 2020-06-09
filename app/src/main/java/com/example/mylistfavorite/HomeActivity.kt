97b253687f4d9faa34676d247bfd6195c3888464
David Melo <David.13rodrigues@hotmail.com>
2020-06-09 02:00:25 -0300

    add webview

--- app/src/main/java/com/example/mylistfavorite/HomeActivity.kt
+++ app/src/main/java/com/example/mylistfavorite/HomeActivity.kt
@@ -1,7 +1,12 @@
 package com.example.mylistfavorite
 
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class HomeActivity() : AppCompatActivity() {
 
@@ -9,7 +14,17 @@ class HomeActivity() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val myWebView = WebView(application)
+        setContentView(myWebView)
+
+        BottomNavigation.setOnClickListener {
+            val myWebView: WebView = findViewById(R.id.webview)
+            myWebView.loadUrl("https://g.co/kgs/nZs5DM")
+        }
+
 
     }
+
+
 
 }
